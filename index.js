2b476950204391bfa8b11f3292c7d292d10614e4
Abdo Mer <91567464+merghemi03@users.noreply.github.com>
2024-03-23 21:53:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.listen(PORT, () => {
 
 app.get('/getVideoURL', async (req, res) => {
     const { videoURL } = req.query;
-    Youtube.getInfo({url: videoURL}).then(video => res.json({ formats:video['formats']}));
-    
+    await Youtube.getInfo({url: videoURL}).then(video => console.log(video));
+    console.log('sss');
 });
