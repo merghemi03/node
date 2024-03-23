5005b4890f0b0fb45f171d81c9dfb13ffb71bde0
Abdo Mer <merghemiabdelhamid2021@gmail.com>
2024-03-23 21:32:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const Youtube = require('youtube-stream-url');
+
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Define your routes and middleware here
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+app.get('/getVideoURL', async (req, res) => {
+    const { videoURL } = req.query;
+    Youtube.getInfo({url: videoURL}).then(video => res.json({ formats:video['formats']}));
+    
+});
